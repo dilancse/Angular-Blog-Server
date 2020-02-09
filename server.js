3a1137144c56c00457bdb02c9f1cc6ba0ce36393
Dilan Nuwantha <dilan.nuwantha@gmail.com>
2020-02-09 22:20:04 +0530

    included articles api return empty array

--- server.js
+++ server.js
@@ -5,3 +5,9 @@ const app = express();
 app.listen(8000, () => {
     console.log("Server is started and listning");
 });
+
+app.get("/", function(request, response){
+    response.send("Hello node.js !");
+});
+
+require("./articles.js")(app);
