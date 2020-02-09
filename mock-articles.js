19754b75d7c7d8870fc7325d74de3fb6267c6d76
Dilan Nuwantha <dilan.nuwantha@gmail.com>
2020-02-09 22:33:47 +0530

    included mock articles and get artical by key api

---
+++ mock-articles.js
@@ -0,0 +1,18 @@
+module.exports = [
+    { id: 1,
+        title: 'First Article',
+        content: 'This is first article content',
+        description: 'this is first article description',
+        key: 'First-Article',
+        date: new Date(),
+        imageUrl: 'http://angular.io/assets/images/logos/angular/angular.png'
+      },
+      { id: 2,
+        title: 'Second Article',
+        content: 'This is Second article content',
+        description: 'this is Second article description',
+        key: 'Second-Article',
+        date: new Date(),
+        imageUrl: 'http://angular.io/assets/images/logos/angular/angular_solidBlack.png'
+      }
+];
